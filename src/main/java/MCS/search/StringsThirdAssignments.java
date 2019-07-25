d27ea5a49c9a664d716dc6c8cc75e96ab7c411da
Andrew8848 <andrey.4ery@gmail.com>
2019-07-25 12:45:15 +0300

    geneFinder-02

--- src/main/java/MCS/search/StringsThirdAssignments.java
+++ src/main/java/MCS/search/StringsThirdAssignments.java
@@ -5,82 +5,22 @@ import java.io.*;
 
 public class StringsThirdAssignments {
 
-//	public int findStopCodon(String dnaStr, int startIndex, String stopCodon) {
-//		int currIndex = dnaStr.indexOf(stopCodon, startIndex + 3);
-//		while (currIndex != -1) {
-//			int diff = currIndex - startIndex;
-//			if (diff % 3 == 0) {
-//				return currIndex;
-//			} else {
-//				currIndex = dnaStr.indexOf(stopCodon, currIndex + 1);
-//			}
-//		}
-//		return -1;
-//
-//	}
-
-//	public String findGene(String dna, int where) {
-//		int startIndex = dna.indexOf("atg", where);
-//
-//		if (startIndex == -1) {
-//			return "";
-//		}
-//
-//		int taaIndexS = dna.indexOf("taa", startIndex + 3);
-//		int tagIndexS = dna.indexOf("tag", startIndex + 3);
-//		int tgaIndexS = dna.indexOf("tga", startIndex + 3);
-//		
-//		int taaIndex;
-//		int tagIndex;
-//		int tgaIndex;
-//		
-//		if ((taaIndexS-startIndex) % 3 == 1) {
-//			taaIndex = taaIndexS;
-//		}else {
-//			taaIndex = -1;
-//		}
-//		
-//		if ((tagIndexS-startIndex) % 3 == 1) {
-//			tagIndex = tagIndexS;
-//		}else {
-//			tagIndex = -1;
-//		}
-//	
-//		if ((tgaIndexS-startIndex) % 3 == 1) {
-//			tgaIndex = tgaIndexS;
-//		}else {
-//			tgaIndex = -1;
-//		}
-
-//		
-//		int minIndex = 0;
-//
-//		if (taaIndex == -1 || (tgaIndex != -1 && tgaIndex < taaIndex)) {
-//			minIndex = tgaIndex;
-//		} else {
-//			minIndex = taaIndex;
-//		}
-//		if (minIndex == -1 || (tagIndex != -1 && tagIndex < minIndex)) {
-//			minIndex = tagIndex;
-//		}
-//		if (minIndex == -1) {
-//			return "";
-//		}
-//
-//		return dna.substring(startIndex, minIndex + 3);
-//	}
+	public int newPosOfStopCodon() {
+		return 0;
+	}
 
 	public int chekGene(String dna, String stopCodon, int startIndex) {
-
 		int stopIndex = dna.indexOf(stopCodon, startIndex + 3);
-		int geneInt = (stopIndex+3) - startIndex;
-		String gene = dna.substring(startIndex, stopIndex+3);
-		
-		if (gene.length() % 3 == 0) {
-			return stopIndex;
-		}
+		// int geneInt = (stopIndex+3) - startIndex;
+		while (true) {
+			String gene = dna.substring(startIndex, stopIndex + 3);
+			if (gene.length() % 3 == 0) {
+				return stopIndex;
 
-			return -1;
+			} else {
+				stopIndex = dna.indexOf(stopCodon, startIndex + gene.length());
+			}
+		}
 	}
 
 	public String findGene(String dna, int posStartIndex) {
@@ -92,9 +32,13 @@ public class StringsThirdAssignments {
 		int stopCodonOfTaa = chekGene(dna, "taa", startIndex);
 		int stopCodonOfTag = chekGene(dna, "tag", startIndex);
 		int stopCodonOfTga = chekGene(dna, "tga", startIndex);
-		
+
 		int minIndex = 0;
-		
+
+		if (stopCodonOfTaa == -1 && stopCodonOfTag == -1 && stopCodonOfTga == -1) {
+
+		}
+
 		if (stopCodonOfTaa == -1 || (stopCodonOfTga != -1 && stopCodonOfTga < stopCodonOfTaa)) {
 			minIndex = stopCodonOfTga;
 		} else {
@@ -106,30 +50,25 @@ public class StringsThirdAssignments {
 		if (minIndex == -1) {
 			return "";
 		}
-		
-		return dna.substring(startIndex, minIndex + 3);
+
+		return dna.substring(startIndex, minIndex+3);
 	}
 
 	public StorageResource printAllGenes(String dna) {
 		StorageResource geneList = new StorageResource();
 		int startIndex = 0;
-	//	int zeroToOne = 0;
-		//boolean toSwitchNum = false;
+
 		while (true) {
-			
-//			if(toSwitchNum == true) {
-//				 zeroToOne = 1;
-//			}
-			
+
 			String currentGene = findGene(dna, startIndex);
-			
+
 			if (currentGene.isEmpty()) {
 				break;
 			}
-			
+
 			geneList.add(currentGene);
-			startIndex = dna.indexOf("atg", (startIndex + currentGene.length()) + 1);
-//            toSwitchNum = true;
+			startIndex = dna.indexOf("atg", startIndex + currentGene.length());
+
 		}
 		return geneList;
 	}
@@ -139,7 +78,6 @@ public class StringsThirdAssignments {
 
 		for (String s : genes.data()) {
 			float result = cgRatio(s);
-
 			System.out.println("cgRatio: " + result);
 			System.out.println("genes: " + s);
 
@@ -170,12 +108,9 @@ public class StringsThirdAssignments {
 
 			FileResource fr = new FileResource(f);
 			String dnaFromDB = fr.asString();
-			String fastTest = "tggcctadtgatggttcagtggattgatttaagttaggtgaggcttatggtcaaggtctaa";
 			System.out.println("dnaFromDB: " + "read " + dnaFromDB.length() + " characters");
 			testOn(dnaFromDB);
-			String ans = findGene(fastTest, 0);
-			System.out.println("     ");
-			System.out.println("fastTest: " + ans);
+			
 		}
 
 	}
--- src/test/java/MCS/search/AppTest.java
+++ src/test/java/MCS/search/AppTest.java
@@ -1,5 +1,6 @@
 package MCS.search;
 
+import edu.duke.StorageResource;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +8,43 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	private String dna = "tggcctadtgatggttcagtggattgatttaagttaggtgaggcttatggtcaaggtctaa";
+	private int i = 0;
+	private boolean trueFalse = true;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public AppTest(String testName) {
+		StringsThirdAssignments sta = new StringsThirdAssignments();
+		StorageResource genes = sta.printAllGenes(dna);
+		for (String s : genes.data()) {
+			
+			if(genes == null) {
+			trueFalse = false;
+			}
+			
+			System.out.println("genes: " + s);
+			
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		}
+		assertTrue(trueFalse);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 }
